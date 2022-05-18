730b1967794e087e39cb6240b4c2f7315946d66f
Yurippe <yurichikawa1992@gmail.com>
2022-05-18 17:12:51 -0400

    add partials and helpers

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
